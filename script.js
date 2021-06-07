df83b2748408c8286492aaaa151d3d6abb815714
lillianlakes <lillian.lakes@gmail.com>
2021-06-07 13:46:48 -0700

    first file commit

---
+++ script.js
@@ -0,0 +1,351 @@
+function emailSubmitted() {
+  var txt;
+  if (confirm("I look forward to keeping you up-to-date on my travel adventures!  By providing your email address, you agree to receive travel update emails from me. Press OK to proceed or cancel if you prefer not to be emailed. Thank you.")) {
+    txt = "Thank you for joining my email list.";
+  } else {
+    txt = "I am sorry you chose not to join my email list.  Please keep in touch.";
+  }
+  document.getElementById("emailConfirmation").innerHTML = txt;
+}
+
+function likeFunction() {
+  alert("Thanks for liking my post!");
+}
+
+function commentFunction1() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment1").innerHTML = txt;
+}
+
+function commentFunction2() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment2").innerHTML = txt;
+}
+
+function commentFunction3() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment3").innerHTML = txt;
+}
+
+function commentFunction4() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment4").innerHTML = txt;
+}
+
+function commentFunction5() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment5").innerHTML = txt;
+}
+
+function commentFunction6() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment6").innerHTML = txt;
+}
+
+function commentFunction7() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment7").innerHTML = txt;
+}
+
+function commentFunction8() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment8").innerHTML = txt;
+}
+
+function commentFunction9() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment9").innerHTML = txt;
+}
+
+function commentFunction10() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment10").innerHTML = txt;
+}
+
+function commentFunction11() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment11").innerHTML = txt;
+}
+
+function commentFunction12() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment12").innerHTML = txt;
+}
+
+function commentFunction13() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment13").innerHTML = txt;
+}
+
+function commentFunction14() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment14").innerHTML = txt;
+}
+
+function commentFunction15() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment15").innerHTML = txt;
+}
+
+function commentFunction16() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment16").innerHTML = txt;
+}
+
+function commentFunction17() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment17").innerHTML = txt;
+}
+
+function commentFunction18() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment18").innerHTML = txt;
+}
+
+function commentFunction19() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment19").innerHTML = txt;
+}
+
+function commentFunction20() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment20").innerHTML = txt;
+}
+
+function commentFunction21() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment21").innerHTML = txt;
+}
+
+function commentFunction22() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment22").innerHTML = txt;
+}
+
+function commentFunction23() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment23").innerHTML = txt;
+}
+
+function commentFunction24() {
+  var txt;
+  var person = prompt("Please share your name:", "First Last");
+  var comment = prompt("Please share your travel experience and other comments:", "I really enjoyed my trip.  I would highly recommend going.");
+  if (person == null || person == "") {
+    txt = "We did not receive your name.  Please try again.";
+  } else if (comment == null || comment == "") {
+    txt = "We did not receive your comment.  Please try again.";
+  } else {
+    txt = `${person}: ${comment}`;
+  }
+  document.getElementById("comment24").innerHTML = txt;
+}
+
+// Can also run scripts for specific element classes e.g. if I wanted to update the number of likes and had a counter.  https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
